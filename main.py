167116c1c49e3bb009a381dfc796fa2228c2b5cc
nirmalp <nirmal.p.jee@gmail.com>
2023-05-28 12:11:37 +0530

    updates

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import *
 import openai
 import os
+import io
+import base64
 from PIL import Image
 from io import BytesIO
 from api.get_calories import getCalories
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = "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"
 
 @app.route("/getData", methods=['GET', 'POST'])
 def getSugestion():
@@ -16,19 +18,20 @@ def getSugestion():
     GENDER = 'Male'
     BMI = 24.9
     AGE = 50
-    # if request.method == 'POST':
     result = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
     messages= [{'role': 'user', 'content': 'Amount of calories required for a {} person with {} and age {} per day. The current food has {} calories. Is it good to have this amount of calorie by not considering amount of calories the person already took. Please provide ways to get required amount of calorie in a day. Don\'t specify you are an AI model. The result should focus on the calorie currently took by the person and the data should be short and up to the point. No need of summary'.format(GENDER, BMI, AGE, calorieCalculated)}],
     temperature=0)
-    getImage('..\\backend\\images')
+    image = getImage('..\\backend\\images')
     response = {
         'result': result["choices"][0]["message"]["content"],
+        'image': image
     }
-    return 
+    return jsonify(response)
 
 def getImage(filePath):
-    img = Image.open(filePath, mode='r')
+    image = Image.open(filePath, mode='r')
     imgByteArr = io.BytesIO()
     imgByteArr = imgByteArr.getvalue()
     imgByteArr = base64.encodebytes(imgByteArr).decode('ascii')
+    return image
